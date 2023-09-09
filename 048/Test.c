95ec55a15dc9b801fe3295739c786999adff914d
diptimishra28 <diptimishra046@gmail.com>
2023-09-09 20:52:20 +0530

    Update the Repository

--- 048/Test.c
+++ 048/Test.c
@@ -1,15 +1,62 @@
+// #include <stdio.h>
+
+// int main(void)
+// {
+//     int n;
+//     printf("Enter a positive integer: ");
+//     scanf("%d", &n);
+
+//     int s;
+//     printf("Enter starting point: ");
+//     scanf("%d", &s);
+
+//     while (s % n != 0)
+//     {
+//         ++s;
+//     }
+//     printf("The next multiple is %d\n", s);
+
+//     return 0;
+// }
+
+/*
+
+Enter a positive integer: 13
+Enter starting point: 50
+The next multiple is 52
+
+Enter a positive integer: 20
+Enter starting point: 40
+The next multiple is 40
+
+*/
+
 #include <stdio.h>
 
 int main(void)
 {
     int n1, n2;
-
     printf("Enter 2 positive integers: ");
-    scanf("%d%d", &n1, &n2);
+    scanf("%d", &n1);
+    scanf("%d", &n2);
 
-    
+    int hcf = 0;
+    int i = 1;
 
+    while ((i <= n1) && (i <= n2))
+    {
+        if ((n1 % i == 0) && (n2 % i == 0))
+        {
+            hcf = i;
+        }
 
+        ++i;
+    }
+
+    int lcm = (n1 * n2) / hcf;
+    printf("LCM = %d\n", lcm);
+
+    return 0;
 
 }
 
--- 048/Test.java
+++ 048/Test.java
@@ -1,4 +1,20 @@
+import java.util.Scanner;
 
+class Test;
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        {
+            int n1, n2;
+            System.out.print("Enter 2 positives: ");
+            n1 = sc.nextInt();
+            n2 = sc.nextInt();
+
+            
+        }
+    }
+}
 
 /*
 1)
