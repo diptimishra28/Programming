19a1540bc00be0232ce2fbd9d9f06334a940d80c
diptimishra28 <diptimishra046@gmail.com>
2023-12-07 20:21:15 +0530

    Update the Repository

---
+++ 112/Test.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+class My_String
+{
+    private char[] arr;
+    private int size;
+    private int capacity;
+
+    My_String()
+    {
+        arr = new char[1];
+        size = 0;
+        capacity = 1;
+    }
+}
+
+class Test
+{
+    public static void main(String[] args)
+    {
+        System.out.println("-------------------------------------------------");
+
+        My_String s1 = new My_String();
+    }
+}
