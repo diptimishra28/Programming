5ee62a5e7ae63222af47021088eb7f6af357a9c1
diptimishra28 <diptimishra046@gmail.com>
2023-12-27 20:21:39 +0530

    Update the Repository

--- 118doubly-linkedlist-2/Test.c
+++ 118doubly-linkedlist-2/Test.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
 
 struct node
 {
 	int value;
+
+    // Node* ptr_previous_node;
+    struct node* ptr_previous_node;
 
 	// Node* ptr_next_node;
 	struct node* ptr_next_node;
@@ -23,9 +25,9 @@ int main(void)
     Node* ptr_head_node = malloc(sizeof (Node));
     scanf("%d", &(ptr_head_node->value));
 
-    ptr_head_node->ptr_next_node = ptr_head_node;
+    ptr_head_node->ptr_next_node = NULL;
 
-    Node* ptr_previous_node = ptr_head_node;
+    ptr_previous_node = ptr_head_node;
 
     for (int i = 1; i < n; ++i)
     {
@@ -33,40 +35,29 @@ int main(void)
         scanf("%d", &(ptr_current_node->value));
 
         ptr_previous_node->ptr_next_node = ptr_current_node;
-        ptr_current_node->ptr_next_node = ptr_head_node;
+        ptr_current_node->ptr_next_node = NULL;
 
         ptr_previous_node = ptr_current_node;
     }
 
     Node* ptr_current_node = ptr_head_node;
 
-    while (true)
+    while (ptr_current_node != NULL)
     {
         printf("%d ", ptr_current_node->value);
         ptr_current_node = ptr_current_node->ptr_next_node;
-
-        if (ptr_current_node == ptr_head_node)
-        {
-            break;
-        }
     }
 
     putchar('\n');
 
     ptr_current_node = ptr_head_node;
 
-    while (true)
+    while (ptr_current_node != NULL)
     {
         Node* ptr_temp = ptr_current_node;
         ptr_current_node = ptr_current_node->ptr_next_node;
         free(ptr_temp);
-
-        if (ptr_current_node == ptr_head_node)
-        {
-            break;
-        }
     }
 
 	return 0;
 }
-
